a9959159a80aebeca87055d911b9c07fd9773322
Jamie <jamie@ncass.org.uk>
2020-09-22 21:45:37 +0100

    Inital Commit

--- SecureClient/AuthConfig.cs
+++ SecureClient/AuthConfig.cs
@@ -10,13 +10,30 @@ namespace SecureClient
         public string Instance { get; set; }
         public string TenantId { get; set; }
         public string ClientId { get; set; }
-
         public string Authority
         {
             get
             {
                 return string.Format(CultureInfo.InvariantCulture, Instance, TenantId);
             }
+        }
+
+        public string ClientSecret { get; set; }
+        public string BaseAddress { get; set; }
+        public string ResourceId { get; set; }
+
+        public static AuthConfig ReadJsonFromFile(string path)
+        {
+            IConfiguration Configuration;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(path);
+
+            Configuration = builder.Build();
+
+            return Configuration.Get<AuthConfig>();
+
         }
     }
 }
--- SecureClient/Program.cs
+++ SecureClient/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
 
 namespace SecureClient
 {
@@ -6,7 +12,70 @@ namespace SecureClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Making call fingers crossed.....");
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task RunAsync()
+        {
+            AuthConfig config = AuthConfig.ReadJsonFromFile("C:\\Repos\\SecureClient\\SecureClient\\appsettings.json");
+            
+            IConfidentialClientApplication app;
+
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithClientSecret(config.ClientSecret)
+                .WithAuthority(new Uri(config.Authority))
+                .Build();
+
+            //Array used if you have more than ResourceId
+            string[] ResourceIds = new string[] {config.ResourceId};
+            AuthenticationResult result = null;
+
+            try
+            {
+                result = await app.AcquireTokenForClient(ResourceIds).ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token Aquired \n");
+                Console.WriteLine(result.AccessToken);
+                Console.ResetColor();
+            }
+            catch (MsalClientException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                var httpClient = new HttpClient();
+                var defaultRequestHeaders = httpClient.DefaultRequestHeaders;
+
+                if (defaultRequestHeaders.Accept == null || !defaultRequestHeaders.Accept.Any
+                    (m => m.MediaType == "application/json"))
+                {
+                    httpClient.DefaultRequestHeaders.Accept.Add(new
+                        MediaTypeWithQualityHeaderValue("application/json"));
+                }
+
+                defaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.AccessToken);
+                HttpResponseMessage response = await httpClient.GetAsync(config.BaseAddress);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    string json = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(json);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Failed to Call API: {response.StatusCode}");
+                    string content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Content: {content}");
+                }
+
+            }
         }
     }
 }
