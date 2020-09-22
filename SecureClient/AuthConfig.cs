4d34caa4666d90c0604c42f57b8c699c43b1322a
Jamie <jamie@ncass.org.uk>
2020-09-22 20:47:59 +0100

    INital Commit

---
+++ SecureClient/AuthConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using Microsoft.Extensions.Configuration;
+
+namespace SecureClient
+{
+    public class AuthConfig
+    {
+        public string Instance { get; set; }
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+
+        public string Authority
+        {
+            get
+            {
+                return string.Format(CultureInfo.InvariantCulture, Instance, TenantId);
+            }
+        }
+    }
+}
